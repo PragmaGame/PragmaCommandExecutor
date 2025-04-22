f68040f7ff8c90d39b22c17e3e479df8057f822b
Zahar <zahar.talai@vaveda.games>
2025-04-22 16:44:07 +0300

    Update ICommandProcessor.cs

--- Assets/PragmaCommandExecutor/ICommandProcessor.cs
+++ Assets/PragmaCommandExecutor/ICommandProcessor.cs
@@ -6,23 +6,22 @@ namespace Pragma.CommandExecutor
 {
     public interface ICommandProcessor<in TCommand> : ICommandProcessor where TCommand : ICommand
     {
-        public UniTask Execute(TCommand command);
-        
+        public UniTask Execute(TCommand command, CancellationToken token);
+
         UniTask ICommandProcessor.Execute(object command, CancellationToken token)
         {
             if (command is not TCommand convert)
             {
                 throw new Exception($"Processor cannot convert {command.GetType()} to target type {typeof(TCommand)}");
             }
-            
+
             return Execute(convert, token);
         }
     }
-    
+
     public interface ICommandProcessor
     {
         public UniTask Execute(object command, CancellationToken token);
-
         public void Interrupt();
     }
 }
