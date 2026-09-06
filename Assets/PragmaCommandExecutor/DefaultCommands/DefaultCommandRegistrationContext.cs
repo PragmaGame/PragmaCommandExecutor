e34378e5d1efd8acc5dc73c2140ae03b12958916
PragmaGame <PragmaGame@users.noreply.github.com>
2026-09-06 13:26:02 +0300

    Added Registration Context

---
+++ Assets/PragmaCommandExecutor/DefaultCommands/DefaultCommandRegistrationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pragma.CommandExecutor
+{
+    public class DefaultCommandRegistrationContext : ICommandRegistrationContext
+    {
+        public IReadOnlyDictionary<Type, Type> Registrations { get; }
+        
+        [UnityEngine.Scripting.RequiredMember]
+        public DefaultCommandRegistrationContext()
+        {
+            Registrations = new Dictionary<Type, Type>
+            {
+                {typeof(DelayCommand), typeof(DelayCommandProcessor)},
+                {typeof(CallbackCommand), typeof(CallbackCommandProcessor)},
+                {typeof(LogCommand), typeof(LogCommandProcessor)},
+                
+                {typeof(ScaleCommand), typeof(ScaleCommandProcessor)},
+            };
+        }
+    }
+}
